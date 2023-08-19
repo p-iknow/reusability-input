fa8298aa3435feefede621c13b73cb18aee5bc15
p-iknow <apricotsoul@gmail.com>
2023-08-19 10:29:19 +0900

    init: first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Input } from "./components/Input";
+
+function App() {
+  return (
+    <div className="App">
+      <Input>
+        <Input.TextField />
+      </Input>
+    </div>
+  );
+}
+
+export default App;
